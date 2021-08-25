5be1f6bb41ec1d1f0be6973c912274836ebf22c9
Sachini Jagodaaracchi <spj@tiqri.com>
2021-08-25 17:01:12 +0300

    Add DeleteProductHandler

---
+++ ShoppingCart/Application/Product/Commands/DeleteProductHandler.cs
@@ -0,0 +1,22 @@
+﻿using Application.Common.Interfaces;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Product.Commands
+{
+    public class DeleteProductHandler : IRequestHandler<DeleteProductCommand, bool>
+    {
+        private readonly IDataAccess dataAccess;
+
+        public DeleteProductHandler(IDataAccess dataAccess)
+        {
+            this.dataAccess = dataAccess;
+        }
+
+        public Task<bool> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(dataAccess.DeleteProduct(request));
+        }
+    }
+}
